b7fd60ebd97661abdc9a7f198e2fc668003b3871
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2020-03-02 19:23:06 +0100

    SC-1 connected main-app to state

--- frontend/src/components/core/Link/Link.tsx
+++ frontend/src/components/core/Link/Link.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavLink = (props: Record<string, any>) => (
-  <div className="nav-link">
-    <i className={props.iconName} />
-    <Link to={props.to}>
+    <Link className="nav-link" to={props.to}>
+
       {
         props.children
       }
     </Link>
-  </div>
 );
 
 export default NavLink;
--- frontend/src/components/unloggedUser/RegisterPage/RegisterPage.tsx
+++ frontend/src/components/unloggedUser/RegisterPage/RegisterPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import RegisterForm from './RegisterForm';
-import { ChangeUserMethods } from '../../../types/generalTypes';
 
-type PropsType = Record<string, any> & ChangeUserMethods;
+type PropsType = Record<string, any>;
 
 class RegisterPage extends React.Component<PropsType, {}> {
 
@@ -10,7 +9,7 @@ class RegisterPage extends React.Component<PropsType, {}> {
     return (
       <div className="register-form-container">
         <h1>Zarejestruj się</h1>
-        <RegisterForm loginUser={this.props.loginUser} />
+        <RegisterForm />
       </div>
     );
   }
